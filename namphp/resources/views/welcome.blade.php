5d50b06a75588f9058a6aeb204529d55157359c1
lenam0811 <lenhatnam0811@gmail.com>
2024-07-06 16:16:46 +0700

    Nam first commit

--- namphp/resources/views/welcome.blade.php
+++ namphp/resources/views/welcome.blade.php
@@ -1,1 +1,21 @@
-<echo ?> nam sieu ngu</echo>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="{{ route('namngu') }}">nam ngu lon</a>
+    <p> </p>
+    <a href="{{ route('namngu1', 'ngao') }}">nam ngu lon1</a>
+    <p></p>
+    <a href="http://127.0.0.1:8000/conga">nam ngu lon2</a>
+
+</body>
+
+</html>
--- namphp/routes/web.php
+++ namphp/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+route::get('/namngu', function(){
+    return 'Anh ';
+})->name('namngu');
+route::get('/namngu/{name}', function($name){
+    return 'Anh ' . $name . '!';
+})->name('namngu1');
+route::get('/conga', function(){
+    return redirect('/');
+});
